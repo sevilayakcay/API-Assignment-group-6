45cafb12a65143e78db79b52b5ba54be32425eb9
SallyResch <sallyresch27@gmail.com>
2025-12-11 12:19:50 +0100

    added js and styling to random meal api fetching

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,35 @@
-//alert("Hello")
+const randomMealApi = "https://www.themealdb.com/api/json/v1/1/random.php";
+const mealOfDayDiv = document.querySelector(".meal-of-the-day_div");
+const mealButton = document.querySelector(".meal-btn");
+const mealImage = document.querySelector(".meal-image");
+const mealName = document.querySelector(".meal-of-the-day-title");
+const fetchRandomMeal = async () => {
+  try {
+    randomMealApi;
+    const response = await fetch(randomMealApi);
+
+    if (!response.ok) {
+      throw new Error("Sry, there is a Network issue");
+    }
+
+    const randomMealData = await response.json();
+    console.log(randomMealData)
+    console.log(randomMealData.meals[0].strMeal);
+    mealImage.setAttribute("src", randomMealData.meals[0].strMealThumb);
+    mealImage.setAttribute("height", 200);
+    mealImage.setAttribute("width", 200);
+    mealName.textContent = randomMealData.meals[0].strMeal;
+  }
+  catch (error) {
+    console.log("Error fetching random meal: ", error);
+  }
+}
+
+const showMealOfDay = () => {
+  mealOfDayDiv.classList.remove("hidden");
+}
+
+mealButton.addEventListener("click", () => {
+  showMealOfDay();
+  fetchRandomMeal();
+})
