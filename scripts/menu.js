c7d3dec638dcd5273e904b74fdd608a158e03b70
mousmimukherjee-dev <mousumi.mukherjee@hyperisland.se>
2025-12-11 11:19:33 +0100

    add:api in menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,1 +1,31 @@
 //alert("menu!");
+
+const button=document.querySelector('.meal-btn');
+const mainPara=document.querySelector('.main-para');
+const dish_img=document.getElementById('dish_img')
+
+
+button.addEventListener('click',async()=>{
+
+
+
+  try{
+
+    const response= await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+    const data=await response.json();
+    const strMealContent =data.meals[0].strMeal;
+    const strThumb=data.meals[0].strMealThumb;
+    mainPara.textContent=strMealContent;
+    dish_img.src=strThumb;
+    console.log(strMealContent);
+    console.log(strThumb);
+
+  }
+
+catch(error){
+
+  console.log('ERROR:' ,error);
+  
+}
+
+});
