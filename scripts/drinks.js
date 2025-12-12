e28a9fa4c68f22a4f2ca4f3097a367ba0aae8f48
mousmimukherjee-dev <mousumi.mukherjee@hyperisland.se>
2025-12-12 04:11:18 +0100

    fix:indentation

--- scripts/drinks.js
+++ scripts/drinks.js
@@ -1,114 +1,79 @@
 //alert("drinks!");
-const btnAlcoholic = document.getElementById('btn-alc');
-const btnNonAlcoholic = document.getElementById('btn-non-alc');
-const btnChefSpecial = document.getElementById('btn-chef-spec');
-const mainContent=document.querySelector('.main-content');
-const drinksContainer=document.getElementById('drinks_container');
+const btnAlcoholic = document.getElementById("btn-alc");
+const btnNonAlcoholic = document.getElementById("btn-non-alc");
+const btnChefSpecial = document.getElementById("btn-chef-spec");
+const mainContent = document.querySelector(".main-content");
+const drinksContainer = document.getElementById("drinks_container");
 
+const getAlcoholicDrink = async () => {
+  console.log("clicked");
 
-const getAlcoholicDrink= async ()=>{
+  drinksContainer.innerHTML = "";
 
-  console.log('clicked');
-
-  drinksContainer.innerHTML='';
-
-  try{
-
-    const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic');
-    const data= await response.json();
-    const drinks=data.drinks.slice(0,10);
-    drinks.forEach(drink => {
-
-      
-
+  try {
+    const response = await fetch(
+      "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic"
+    );
+    const data = await response.json();
+    const drinks = data.drinks.slice(0, 10);
+    drinks.forEach((drink) => {
       console.log(drink);
-      const display= document.createElement('div');
-      display.classList.add('display');
-      display.innerHTML=`<img src='${drink.strDrinkThumb}'
+      const display = document.createElement("div");
+      display.classList.add("display");
+      display.innerHTML = `<img src='${drink.strDrinkThumb}'
       <p>${drink.strDrink}</p>`;
       drinksContainer.appendChild(display);
-
-      
     });
-    
-
-
-  }
-
-  catch(error){
-
-    console.log('Error:',error);
-    
+  } catch (error) {
+    console.log("Error:", error);
   }
 };
 
-const getNonAlcoholicDrink= async ()=>{
+const getNonAlcoholicDrink = async () => {
+  console.log("clicked");
+  drinksContainer.innerHTML = "";
 
-  console.log('clicked');
-  drinksContainer.innerHTML='';
-
-  try{
-
-    const res = await fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic');
+  try {
+    const res = await fetch(
+      "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic"
+    );
     const data = await res.json();
-    const drinks=data.drinks.slice(0,10);
-    drinks.forEach(drink=>{
-
+    const drinks = data.drinks.slice(0, 10);
+    drinks.forEach((drink) => {
       console.log(drink.strDrink);
-      const display=document.createElement('div');
+      const display = document.createElement("div");
 
-      display.innerHTML=`<img src=${drink.strDrinkThumb}>
+      display.innerHTML = `<img src=${drink.strDrinkThumb}>
       <p>${drink.strDrink}</p>`;
       drinksContainer.appendChild(display);
-
-    })
-    
-
+    });
+  } catch (error) {
+    console.log("Error:", error);
   }
+};
 
-  catch(error){
+const getChefSpecial = async () => {
+  console.log("clicked");
 
-    console.log('Error:',error);
-  }
-}
+  drinksContainer.innerHTML = "";
 
-
-
-
-
-const getChefSpecial= async ()=>{
-
-  console.log('clicked');
-
-  drinksContainer.innerHTML='';
-
-  try{
-
-    const res = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+  try {
+    const res = await fetch(
+      "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+    );
     const data = await res.json();
-    const drinks=data.drinks[0];
-    
-      const display=document.createElement('div');
+    const drinks = data.drinks[0];
 
-      display.innerHTML=`<img src=${drinks.strDrinkThumb}>
+    const display = document.createElement("div");
+
+    display.innerHTML = `<img src=${drinks.strDrinkThumb}>
       <p>${drinks.strDrink}</p>`;
-      drinksContainer.appendChild(display);
-
-    
-    
-
+    drinksContainer.appendChild(display);
+  } catch (error) {
+    console.log("Error:", error);
   }
+};
 
-  catch(error){
-
-    console.log('Error:',error);
-  }
-
-  
-}
-
-
-
-btnAlcoholic.addEventListener('click',getAlcoholicDrink);
-btnNonAlcoholic.addEventListener('click',getNonAlcoholicDrink);
-btnChefSpecial.addEventListener('click',getChefSpecial);
+btnAlcoholic.addEventListener("click", getAlcoholicDrink);
+btnNonAlcoholic.addEventListener("click", getNonAlcoholicDrink);
+btnChefSpecial.addEventListener("click", getChefSpecial);
